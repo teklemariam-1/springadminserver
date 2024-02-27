f6c4fcaffc1cc68142482e6e5d28679eab9a9b3f
teklemariam <tek.teweldeibrhan@gmail.com>
2024-02-27 00:46:26 -0500

    Add Spring Boot Admin Server setup
    Adds the basic setup for the Spring Boot Admin Server, including necessary configurations in the application.properties file and the main SpringadminserverApplication file. The relevant Maven wrapper files and gitignore are also added.

---
+++ src/test/java/com/tek/springadminserver/SpringadminserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tek.springadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringadminserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
